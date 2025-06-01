dacc5496677ee4e0ee445922769375635d21046b
MikhailLukach <mikhail.r.lukach@gmail.com>
2025-06-01 22:11:57 +0200

    Updated NobbinComponent
    Updated nobbincomponent to try and get the jitters out of the project. I think it might be something to do with the constant debug logs.

--- Digger/NobbinComponent.cpp
+++ Digger/NobbinComponent.cpp
@@ -8,15 +8,20 @@ void dae::NobbinComponent::Update(float deltaTime)
 	if (!m_IsMoving) return;
 
 	m_MoveElapsed += deltaTime;
-	float t = std::min(m_MoveElapsed / m_MoveDuration, 1.f);
-	glm::vec3 newPos = glm::mix(m_StartPosition, m_TargetPosition, t);
-	GetOwner()->GetTransform()->SetWorldPosition(newPos);
 
+	float t = m_MoveElapsed / m_MoveDuration;
 	if (t >= 1.f)
 	{
+		GetOwner()->GetTransform()->SetWorldPosition(m_TargetPosition); // snap exactly
 		m_IsMoving = false;
+		m_MoveElapsed -= m_MoveDuration; //preserve excess time for next move
+
 		std::cout << "[Nobbin] Move complete to tile.\n";
+		return;
 	}
+
+	glm::vec3 newPos = glm::mix(m_StartPosition, m_TargetPosition, t);
+	GetOwner()->GetTransform()->SetWorldPosition(newPos);
 }
 
 void dae::NobbinComponent::NobbinStartMoveTo(const glm::vec3& targetPos, float duration)
